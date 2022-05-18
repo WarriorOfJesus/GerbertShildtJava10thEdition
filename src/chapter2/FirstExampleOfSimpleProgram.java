8b506826e89e9183a0929434cf0ea751f6d7c81f
WarriorOfJeses <kucovaleksej@gmail.com>
2022-05-18 19:11:34 +0300

    new class

--- src/chapter2/FirstExampleOfSimpleProgram.java
+++ src/chapter2/FirstExampleOfSimpleProgram.java
@@ -2,6 +2,7 @@ package chapter2;
 
 public class FirstExampleOfSimpleProgram {
     public static void main(String[] args) {
+        System.out.println("first example of simple pogram");
 
     }
 }
